9f97a5394451046b13fc52d8213182af2ced374a
Justin Byrd <justinbyrd7@gmail.com>
2023-12-19 10:11:53 -0600

    Add tests for getAll method in hobbits-model.js

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -1,3 +1,30 @@
+const db = require("../../data/dbConfig.js");
+const Hobbit = require("./hobbits-model.js");
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+beforeEach(async () => {
+  await db.seed.run();
+});
+
 test("environment is testing", () => {
   expect(process.env.NODE_ENV).toBe("testing");
+});
+
+describe("getAll", () => {
+  test("resolves all the hobbits in the table", async () => {
+    const result = await Hobbit.getAll();
+    expect(result).toHaveLength(4);
+    expect(result[0]).toMatchObject({ name: "sam" });
+    expect(result[1]).toMatchObject({ name: "frodo" });
+    expect(result[2]).toMatchObject({ name: "pippin" });
+    expect(result[3]).toMatchObject({ name: "merry" });
+  });
+});
+
+describe("getById", () => {
+  test("resolves hobbit by id from table", async () => {});
 });
